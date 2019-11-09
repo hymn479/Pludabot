361a5e7d853c26c1b73340ba0e579336e9e053c6
hymn479 <57278807+hymn479@users.noreply.github.com>
2019-11-09 14:39:55 +0900

    Add files via upload

---
+++ my_bot.py
@@ -0,0 +1,28 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("botstart")
+    game = discord.Game("맞장뜨실")
+    await client.change_presence(status=discord.Status.idle, activity=game)
+
+    @client.event
+    async def on_message(message):
+        if message.content.startswith("지니바보"):
+            await message.channel.send("어쩔안물")
+        if message.content.startswith("!강화"):
+            await message.channel.send("45%의 확률로 Lv1>Lv5로 강화됬습니다! 강화대기시간:30초")
+
+
+
+
+
+
+client.run("NTUwODgxNzAxNTQ3NzM3MDg4.XcZJwA.Httg_Du9AxiF4CRcBLEFd5ZCsYU")
+
+
+
+            
